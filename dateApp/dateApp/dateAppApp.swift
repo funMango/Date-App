5ce04de5ac4656b26e1db2bbd0b78ff78c7ec7eb
funMango <138420832+funMango@users.noreply.github.com>
2024-01-18 21:48:06 +0900

    first commit

---
+++ dateApp/dateApp/dateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dateAppApp.swift
+//  dateApp
+//
+//  Created by 이민호 on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct dateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
